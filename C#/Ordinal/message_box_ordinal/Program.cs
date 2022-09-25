65aad5b66e4b89a15c73c20371af99b99e8feb4e
Steve S <santos.s.salinas@gmail.com>
2022-09-25 08:59:34 -0700

    Add files via upload

---
+++ C#/Ordinal/message_box_ordinal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Ordinals
+{
+    internal class Program
+    { //WE USE PEVIEW -> SECTION .TEXT -> EXPORT ADDRESS TABLE -> MESSAGEBOXW TO FIND ORDINAL 0X0874 -> 2164
+        [DllImport("user32.dll", EntryPoint = "#2164", CharSet = CharSet.Unicode)]
+        static extern int NotAMessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+        static void Main(string[] args)
+        {
+            NotAMessageBox(IntPtr.Zero, "Ordinal!!", "Ordinal", 0);
+        }
+
+    }
+
+
+}
