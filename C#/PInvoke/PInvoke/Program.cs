3141de84551a67815971a34a8ebd2c04380f3f02
Steve S <santos.s.salinas@gmail.com>
2022-09-25 08:09:49 -0700

    Add files via upload

---
+++ C#/PInvoke/PInvoke/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PInvoke
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern int MessageBoxW(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+        static void Main(string[] args)
+        {
+            MessageBoxW(IntPtr.Zero, "My first P/Invoke", "Hello World!", 0);
+        }
+
+    }
+
+
+}
