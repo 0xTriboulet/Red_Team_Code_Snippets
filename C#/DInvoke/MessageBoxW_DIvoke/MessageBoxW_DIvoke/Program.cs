6d1cb9a9007d944296255e21e1725e61a6bacada
Steve S <santos.s.salinas@gmail.com>
2022-10-07 14:11:42 -0400

    Add files via upload

---
+++ C#/DInvoke/MessageBoxW_DIvoke/MessageBoxW_DIvoke/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using DInvoke.DynamicInvoke;
+
+namespace MessageBoxW_DInvoke
+{
+    class Program
+    {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+        delegate int MessageBoxW(IntPtr hWnd, string lpText, string pCaption, uint uType);
+        static void Main(string[] args)
+        {
+            var parameters = new object[] { IntPtr.Zero, "My first D/Invoke!", "Hello world!", (uint)0 };
+            Generic.DynamicAPIInvoke("user32.dll", "MessageBoxW", typeof(MessageBoxW), ref parameters);
+
+        }
+    }
+}
